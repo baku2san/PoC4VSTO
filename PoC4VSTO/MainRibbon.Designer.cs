1f182cda06edcf60ed369c163b9f86daf43531e2
uka2san <uka2san@gmail.com>
2022-01-18 22:51:53 +0900

    Excel.Application.InputBox の機能確認

---
+++ PoC4VSTO/MainRibbon.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace PoC4VSTO
+{
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.ButtonPoC4InputBox = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.ButtonPoC4InputBox);
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.button3);
+            this.group1.Items.Add(this.button4);
+            this.group1.Items.Add(this.button5);
+            this.group1.Items.Add(this.button6);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // ButtonPoC4InputBox
+            // 
+            this.ButtonPoC4InputBox.Label = "数式";
+            this.ButtonPoC4InputBox.Name = "ButtonPoC4InputBox";
+            this.ButtonPoC4InputBox.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonPoC4InputBox_Click);
+            // 
+            // button1
+            // 
+            this.button1.Label = "数値";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Label = "文字列";
+            this.button2.Name = "button2";
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Label = "論理値";
+            this.button3.Name = "button3";
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Label = "セル参照";
+            this.button4.Name = "button4";
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Label = "エラー";
+            this.button5.Name = "button5";
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Label = "配列";
+            this.button6.Name = "button6";
+            this.button6.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button6_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonPoC4InputBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MainRibbon MainRibbon
+        {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
---
+++ PoC4VSTO/MainRibbon.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PoC4VSTO
+{
+    public partial class MainRibbon
+    {
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+        enum TypeValue
+        {
+            Formula = 0,	// 数式
+            Number = 1,		// 数値
+            String = 2,		// 文字列 (テキスト)
+            Bool = 4,		// 論理値 (True または False)
+            Range = 8,		// セル参照 (Range オブジェクト)
+            Errors = 16,	// #N/A などのエラー値
+            Array = 64,		// 値の配列
+        }
+        private void ButtonPoC4InputBox_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Formula.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Formula);
+            MessageBox.Show(result);
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Number.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Number);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.String.ToString(), "Type 比較", "default", 50, 50, Type.Missing, Type.Missing, TypeValue.String);
+            MessageBox.Show(result);
+        }
+
+        private void button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Bool.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Bool);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button4_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Range.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Range);
+            MessageBox.Show(result.Address.ToString());
+        }
+
+        private void button5_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Errors.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Errors);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button6_Click(object sender, RibbonControlEventArgs e)
+        {
+            var result = Globals.ThisAddIn.Application.InputBox(TypeValue.Array.ToString(), "Type 比較", Type.Missing, 50, 50, Type.Missing, Type.Missing, TypeValue.Array);
+            MessageBox.Show(result.GetType().ToString());
+        }
+    }
+}
